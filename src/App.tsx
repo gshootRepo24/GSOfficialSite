2b023d4c64c143c042b693dae6f0845a9561891a
omnagar860 <omnagar860@gmail.com>
2025-03-25 22:05:55 +0530

    changes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,54 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
-import RootLayout from "./Components/RootLayout"
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./Components/RootLayout";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
 import GsImage from "./Components/GsImage/GsSysImage";
 import Mfile from "./Components/Mfile/Mfile";
 import Gsdocs from "./Components/GsDocs/GsDocs";
+import GsAbout from "./Components/About/GsAbout";
 import GSWebScan from "./Components/GSWebScan/GSWebScan";
+import { ThemeProvider } from "./Components/ThemesProvider/ThemeProvider";
 
-const theme = createTheme({
-  typography: {
-    fontFamily: "Winky Sans, sans-serif",
-    fontSize:18,
-  },
-});
+
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: "Winky Sans, sans-serif",
+//     fontSize: 18,
+//     fontWeightLight: 300,
+//   },
+//   components: {
+//     MuiCssBaseline: {
+//       styleOverrides: {
+//         body: {
+//           color: "#ffff", // Apply color globally
+//         },
+//       },
+//     },
+//   },
+// });
+
 export default function App() {
-
-  console.log("i am loadign");
+  // console.log("i am loadign");
   const router = createBrowserRouter([
-    {path:'/',element:<RootLayout/>},
-    {path:'/servostreams',element:<RootLayout/>},
-    { path: '/Servoimage', element:<GsImage/> },
-    { path: '/Servodocs', element:<Gsdocs/> },
+    { path: "/", element: <RootLayout /> },
+    { path: "/servostreams", element: <RootLayout /> },
+    { path: "/Nav/GsImage", element: <GsImage /> },
+    { path: "/Nav/GsDocs", element: <Gsdocs /> },
+    { path: "/Nav/GSFiles", element: <Mfile /> },
     { path: '/ServoWebScan', element:<GSWebScan/> },
-    { path: '/mfile', element:<Mfile/> }
-  ])
-  
+    { path: "/Nav/About-Us", element: <GsAbout /> },
+  ]);
+
   return (
-    
-    <ThemeProvider theme={theme}>
-       <RouterProvider router={router} />
+    // // <ThemeProvider theme={theme}>
+    // {/* <GSThemeProvider> */}
+      
+    // {/* </GSThemeProvider> */}
+    // //  </ThemeProvider>
+    <>
+    <ThemeProvider>
+    <RouterProvider router={router} />
     </ThemeProvider>
    
-  )
+    </>
+  );
 }
